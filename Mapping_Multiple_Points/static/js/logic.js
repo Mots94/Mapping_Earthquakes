bead4b7d03bcca7758ce3705d890a5e9e4609937
Mots94 <rmotykow@gmail.com>
2022-06-02 15:40:33 -0500

    completed skill drill

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -6,10 +6,10 @@ console.log("working");
 
 let map = L.map("mapid").setView([40.7, -94.5], 4);
 
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/streets-v11',
+    id: 'mapbox/dark-v10',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: apiKey
@@ -22,7 +22,12 @@ let cityData = cities;
 // Loop through the cities array and create one marker for each city.
 cities.forEach(function(city) {
     console.log(city)
-    L.marker(city.location)
-    .bindPopup(`<strong>City: ${city.city}</strong> <hr> <strong>Population: ${city.population}</strong>`)
+    L.circleMarker(city.location, {
+        radius: city.population/200000,
+        color: "orange",
+        weight: 4
+
+    })
+    .bindPopup(`<strong>${city.city}, ${city.state}</strong> <hr> <strong>Population: ${city.population}</strong>`)
     .addTo(map);
    });
