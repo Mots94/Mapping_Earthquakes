d667190491a0a2655f734ee068ce766874a2b686
Mots94 <rmotykow@gmail.com>
2022-06-03 16:30:39 -0500

    using pointToLayer to modify single map point

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -1,9 +1,4 @@
-// Add console.log to check to see if our code is working
-
-console.log("working");
-
 //Create the map object wtih a center and zoom level
-
 let map = L.map("mapid").setView([37.5, -122.5], 10);
 
 // Add GeoJSON data.
@@ -26,12 +21,19 @@ let sanFranAirport =
             "coordinates":[-122.375,37.61899948120117]}}
 ]};
 
-L.geoJSON(sanFranAirport).addTo(map);
+L.geoJSON(sanFranAirport, {
+    //Each feature is turned into a marker on map
+    pointToLayer: function(feature, latlng) {
+        console.log(feature);
+        return L.marker(latlng)
+        .bindPopup(`<strong>${feature.properties.name}</strong><hr><h4>${feature.properties.city}, ${feature.properties.country}</h4>`)
+    }
+}).addTo(map);
 
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/streets-v11',
+    id: 'mapbox/navigation-night-v1',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: apiKey
