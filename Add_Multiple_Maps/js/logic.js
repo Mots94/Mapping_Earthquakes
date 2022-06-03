bd87f6287f9ccd7dc0b8ccbf69fbf911c4eb3d91
Mots94 <rmotykow@gmail.com>
2022-06-03 18:33:41 -0500

    initial branch file commit

---
+++ Add_Multiple_Maps/js/logic.js
@@ -0,0 +1,35 @@
+//Create the tile layers for two different map types
+
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: apiKey
+});
+
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/dark-v10',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: apiKey
+});
+
+//Create base layer that holds both maps
+
+let baseMaps = {
+    Street: streets,
+    Dark: dark
+};
+
+let map = L.map("mapid", {
+    center: [40.7, -94.5],
+    zoom: 4,
+    layers: [streets]
+});
+
+L.control.layers(baseMaps).addTo(map);
+
