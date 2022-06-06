826f941f31c26ac770952b0d3377695018114447
Mots94 <rmotykow@gmail.com>
2022-06-06 13:44:02 -0500

    finished deliverable 3

--- Earthquake_Challenge/static/js/logicChallenge.js
+++ Earthquake_Challenge/static/js/logicChallenge.js
@@ -18,11 +18,22 @@ let satellite = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}
     accessToken: apiKey
 });
 
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/dark-v10',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: apiKey
+});
+
+
 //Create base layer that holds both maps
 
 let baseMaps = {
     Streets: streets,
-    Satellite: satellite
+    Satellite: satellite,
+    Dark: dark
 };
 
 //Create earthquake layer for the map
